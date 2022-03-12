0abd05e4fe46cf6708a216bd5ab06a8d439b43db
Mini-26 <app@gmail.com>
2022-03-11 16:26:36 -0800

    Add initialize and children to tic tac toe node

---
+++ tic_tac_toe_ai/lib/tic_tac_toe_node.rb
@@ -0,0 +1,37 @@
+require_relative 'tic_tac_toe'
+
+class TicTacToeNode
+    attr_reader :board, :next_mover_mark, :prev_move_pos
+    def initialize(board, next_mover_mark, prev_move_pos = nil)
+        @board = board
+        @next_mover_mark = next_mover_mark
+        @prev_move_pos = prev_move_pos
+    end
+
+    def losing_node?(evaluator)
+    end
+
+    def winning_node?(evaluator)
+    end
+
+    def other_mark
+        next_mover_mark == :x ? :o : :x
+    end
+
+    # This method generates an array of all moves that can be made after
+    # the current move.
+    def children
+        moves = []
+        board.rows.each_with_index do |row, i|
+            row.each_with_index do |space, j|
+                if !space
+                    duped_board = board.dup
+                    duped_board[[i, j]] = next_mover_mark
+                    node = TicTacToeNode.new(duped_board, other_mark, [i, j])
+                    moves << node
+                end
+            end
+        end
+        moves
+    end
+end
