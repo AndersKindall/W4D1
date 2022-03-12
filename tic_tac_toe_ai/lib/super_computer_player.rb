3f301cea8ac7f3397df9eee66774350906920101
Mini-26 <app@gmail.com>
2022-03-11 16:56:20 -0800

    Add move to super computer player class

--- tic_tac_toe_ai/lib/super_computer_player.rb
+++ tic_tac_toe_ai/lib/super_computer_player.rb
@@ -1,14 +1,22 @@
 require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
-  def move(game, mark)
-  end
+    def move(game, mark)
+        node = TicTacToeNode.new(game.board, mark)
+        node.children.each do |child|
+            return child.prev_move_pos if child.winning_node?(mark)
+        end
+        node.children.each do |child|
+            return child.prev_move_pos unless child.losing_node?(mark)
+        end
+        raise "There are no non-losing nodes"
+    end
 end
 
 if __FILE__ == $PROGRAM_NAME
-  puts "Play the brilliant computer!"
-  hp = HumanPlayer.new("Jeff")
-  cp = SuperComputerPlayer.new
+    puts "Play the brilliant computer!"
+    hp = HumanPlayer.new("Jeff")
+    cp = SuperComputerPlayer.new
 
-  TicTacToe.new(hp, cp).run
+    TicTacToe.new(hp, cp).run
 end
